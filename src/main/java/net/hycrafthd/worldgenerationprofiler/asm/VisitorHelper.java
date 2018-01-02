975ef2ebd523b066ee6594f8082ece9a8a9971c9
HyCraftHD <hycrafthd@live.de>
2018-01-02 08:00:21 +0100

    Final 1.0.0

--- src/main/java/net/hycrafthd/worldgenerationprofiler/asm/VisitorHelper.java
+++ src/main/java/net/hycrafthd/worldgenerationprofiler/asm/VisitorHelper.java
@@ -42,16 +42,16 @@ public class VisitorHelper {
 			
 			// ------------------
 			// ONLY DEBUG
-			try {
-				FileOutputStream stream = new FileOutputStream(new File("GameRegistry.class"));
-				stream.write(writer.toByteArray());
-				stream.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			// try {
+			// FileOutputStream stream = new FileOutputStream(new File("GameRegistry.class"));
+			// stream.write(writer.toByteArray());
+			// stream.close();
+			// } catch (FileNotFoundException e) {
+			// e.printStackTrace();
+			// } catch (IOException e) {
+			// // TODO Auto-generated catch block
+			// e.printStackTrace();
+			// }
 			// ------------------
 			
 			return writer.toByteArray();
