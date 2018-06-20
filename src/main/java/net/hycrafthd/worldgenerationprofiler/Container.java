06919b29d6f0c4df4be68012f9940f86795a1e50
HyCraftHD <hycrafthd@live.de>
2018-06-20 15:17:46 +0200

    Commit forced, because of new build

--- src/main/java/net/hycrafthd/worldgenerationprofiler/Container.java
+++ src/main/java/net/hycrafthd/worldgenerationprofiler/Container.java
@@ -1,5 +1,7 @@
 package net.hycrafthd.worldgenerationprofiler;
 
+import java.net.*;
+
 import com.google.common.eventbus.EventBus;
 
 import net.minecraftforge.fml.common.*;
@@ -12,12 +14,23 @@ public class Container extends DummyModContainer {
 		meta.modId = "worldgenerationprofiler";
 		meta.name = "World Generation Profiler";
 		meta.version = "@VERSION@";
-		meta.description = "https://www.curseforge.com/minecraft/mc-mods/worldgeneration-profiler";
+		meta.url = "https://www.curseforge.com/minecraft/mc-mods/worldgeneration-profiler";
+		meta.authorList.add("HyCraftHD");
+		meta.description = "Mod which profiles your worldgeneration";
 	}
 	
 	@Override
 	public boolean registerBus(EventBus bus, LoadController controller) {
 		return true;
+	}
+	
+	@Override
+	public URL getUpdateUrl() {
+		try {
+			return new URL("https://api.u-team.info/update/worldgenerationprofiler.json");
+		} catch (MalformedURLException ex) {
+			return null;
+		}
 	}
 	
 }
